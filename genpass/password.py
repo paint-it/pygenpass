be3a84ed6ef03c4b625a163c78ded87cfc324f02
mayuri <mayuri@localhost.localdomain>
2019-12-19 20:46:12 +0530

    getting input from user

---
+++ genpass/password.py
@@ -0,0 +1,10 @@
+a=int(input("Enter length of password  \n " ))
+print("Lenth of password=  %d   "%(a))
+
+b=input("Enter charactors that you want \n ")
+print("Charactors are =    "+b)
+
+c=input("Enter spcial charactors that you want \n ")
+print("spcial charatos are  "+c)
+
+print("Thank you for input we are processing your password to make it more secure !!!!")
