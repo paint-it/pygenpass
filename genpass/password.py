9f82c3cfaf67d645c49d60b2f337281df5e859eb
mayuri <mayuri@localhost.localdomain>
2019-12-22 19:35:42 +0530

    Input from user

--- genpass/password.py
+++ genpass/password.py
@@ -1,10 +1,31 @@
-a=int(input("Enter length of password  \n " ))
-print("Lenth of password=  %d   "%(a))
+import click
+import diceware
+from datetime import datetime
 
-b=input("Enter charactors that you want \n ")
-print("Charactors are =    "+b)
+@click.group()
+def main():
+    pass
 
-c=input("Enter spcial charactors that you want \n ")
-print("spcial charatos are  "+c)
 
-print("Thank you for input we are processing your password to make it more secure !!!!")
+@main.command(help="Enter required data")
+def create_pass():
+    portal_name = click.prompt('Enter portal name', default="None")
+    portal_url = click.prompt('Enter portal_url',default="None")
+    user_email = click.prompt('Enter mail-id',default="None")
+    tag = click.prompt('Enter tags',default="None")
+    notes = click.prompt('Enter notes',default="None")
+    password = diceware.get_passphrase()
+    creation_date = datetime.now()
+    print(f"\n\nPortal Name: {portal_name}\n Portal URL: {portal_url}\n User Email: {user_email}\n Tags: {tag}\n Notes: {notes}\n Password: {password}\n Creation Date: {creation_date}")
+
+main.add_command(create_pass)
+
+@main.command(help="printing data")
+def print_data():
+    pass
+
+main.add_command(print_data)
+
+
+if __name__ == '__main__':
+    main()
