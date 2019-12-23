c9964aadfaf193c314171d47029b7f7f9faae1a0
Mayuri Lahane <mayurilahane1998@gmail.com>
2019-12-23 21:10:31 +0530

    encryption

--- genpass/password.py
+++ genpass/password.py
@@ -1,10 +1,23 @@
 import click
 import diceware
 from datetime import datetime
+from passlib.context import CryptContext
+
 
 @click.group()
 def main():
     pass
+
+pwd_context = CryptContext(
+        schemes=["pbkdf2_sha256"],
+        default="pbkdf2_sha256",
+        pbkdf2_sha256__default_rounds=30000
+)
+
+# password encryption
+def encrypt_pass():
+	password = diceware.get_passphrase()
+	return pwd_context.encrypt(password)
 
 
 @main.command(help="Enter required data")
@@ -14,10 +27,9 @@ def create_pass():
     user_email = click.prompt('Enter mail-id',default="None")
     tag = click.prompt('Enter tags',default="None")
     notes = click.prompt('Enter notes',default="None")
-    password = diceware.get_passphrase()
+    password = encrypt_pass()
     creation_date = datetime.now()
-    print(f"\n\nPortal Name: {portal_name}\n Portal URL: {portal_url}\n User Email: {user_email}\n Tags: {tag}\n Notes: {notes}\n Password: {password}\n Creation Date: {creation_date}")
-
+    print("\n\n Portal Name: {portal_name}\n Portal URL: {portal_url}\n User Email: {user_email}\n Tags: {tag}\n Notes: {notes}\n Password: {password}\n Creation Date: {creation_date}".format(portal_name=portal_name, portal_url=portal_url, user_email=user_email, tag=tag, notes=notes, password=password, creation_date=creation_date))
 main.add_command(create_pass)
 
 @main.command(help="printing data")
