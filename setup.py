475d77ef0356014b4c4dfc1c631c64cf53a3fa20
Mayuri Lahane <mayurilahane1998@gmail.com>
2020-01-08 20:11:03 +0530

    Version added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.rst") as readme_file:
     long_description = readme_file.read()
 
-install_requires = ["setuptools", "diceware", "click"]
+install_requires = ["setuptools", "click>=5.0", "diceware>=0.9.5"]
 
 setup_requirements = ["setuptools_scm"]
 
@@ -11,6 +11,7 @@ setup(
     name="pygenpass",
     packages=find_packages(),
     entry_points={"console_scripts": ["pygenpass = genpass.__init__:main"]},
+    version="v0.1",
     author="Mayuri Lahane",
     author_email="mayurilahane1998@gmail.com",
     description="Genpass - Command Line Password Manager Tool",
